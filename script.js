6f516dfd42be1310c488630d4654ac3548c06861
jenDoeMoldovan <michalevic@gmail.com>
2021-10-15 14:09:13 +0300

    some stylistic changes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const catImg = document.querySelector("img");
+const catArray = [];
+
 async function fetchCats() {
   const res = fetch("https://api.thecatapi.com/v1/images/search");
   res
@@ -7,23 +9,26 @@ async function fetchCats() {
     })
     .then(function (cat) {
       showCat(cat);
+      console.log(catArray);
     });
 }
+
 function showCat(cat) {
+  catImg.classList.add("images");
   catImg.src = cat[0].url;
-  catImg.style.width = "50%";
-  catImg.style.height = "50%";
   catArray.push(cat[0].url);
-  console.log(catArray);
 }
-const catArray = [];
+
 document.querySelector("button").onclick = fetchCats;
+
 document.querySelector("button").addEventListener(
   "click",
   () => {
     const prevBtn = document.createElement("button");
     prevBtn.textContent = "Previous cat";
-    document.body.insertBefore(prevBtn, document.querySelector("button"));
+    document
+      .querySelector(".buttons")
+      .insertBefore(prevBtn, document.querySelector("button"));
     prevBtn.onclick = function () {
       // const currentLoc = catArray.indexOf(catImg.src);
       catImg.src = catArray[catArray.indexOf(catImg.src) - 1];
